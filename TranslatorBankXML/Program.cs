e4583a5601f697be2c1516ecb9cb211571eeb04c
steffen <helbo15@gmail.com>
2016-10-22 21:03:35 +0200

    added a class file

--- TranslatorBankXML/Program.cs
+++ TranslatorBankXML/Program.cs
@@ -10,7 +10,18 @@ namespace TranslatorBankXML
     {
         static void Main(string[] args)
         {
-
+            bool running = true;
+            RabbitMQTranslator aTranslator = new RabbitMQTranslator();
+            while(running)
+            {
+                aTranslator.translate();
+                Console.WriteLine("exit? {yes/[no]}: ");
+                string exitkeys = Console.ReadLine();
+                if(exitkeys.Equals("yes"))
+                {
+                    running = false;
+                }
+            }
         }
     }
 }
---
+++ TranslatorBankXML/RabbitMQTranslator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace TranslatorBankXML
+{
+    public class RabbitMQTranslator
+    {
+        public RabbitMQTranslator()
+        {
+
+        }
+        public void translate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
